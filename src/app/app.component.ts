bf691eefb29367cc0309d596abe66df1748b5ce4
Samuel Knoch <samuel@korntal.eu>
2022-06-29 18:18:58 +0200

    lading page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,8 @@
-import { AfterViewInit, Component, ElementRef, NgZone, ViewChild } from '@angular/core';
-import { App } from './core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
 })
-export class AppComponent implements AfterViewInit {
-  public error: Error | null = null;
-  public app!: App;
-  public fps: string = '0';
-
-  @ViewChild('canvas', { static: true }) private canvas?: ElementRef<HTMLCanvasElement>;
-
-  constructor(private readonly ngZone: NgZone) {}
-
-  public async ngAfterViewInit(): Promise<void> {
-    if (this.canvas) {
-      try {
-        this.app = new App(this.canvas.nativeElement, this.ngZone);
-        this.app.startRender();
-        setInterval(() => {
-          this.fps = this.app.fps;
-        }, 10);
-      } catch (e: any) {
-        console.error(e);
-        this.error = e;
-      }
-    }
-  }
-
-  public enterXR(): void {
-    this.app.enterXRMode();
-  }
-}
+export class AppComponent {}
